99444ce40ec205c2c81479a6f7b90ace6e9a755f
faroukq1 <faroukq1@gmail.com>
2024-09-18 15:14:52 +0100

    creating navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar/Navbar";
+import Container from "@/components/global/Container";
 
 export const metadata: Metadata = {
   title: "Nest Store",
@@ -16,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        <Container>{children}</Container>
+      </body>
     </html>
   );
 }
