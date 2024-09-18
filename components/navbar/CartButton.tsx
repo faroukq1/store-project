6d1a4bb8e63ac991cd972093e4f0e14667a51dc4
faroukq1 <faroukq1@gmail.com>
2024-09-18 15:28:25 +0100

    navbar cart

--- components/navbar/CartButton.tsx
+++ components/navbar/CartButton.tsx
@@ -1,5 +1,25 @@
-const CartButton = () => {
-  return <div>CartButton</div>;
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { FaShoppingCart } from "react-icons/fa";
+
+const CartButton = async () => {
+  // here rending how much items in the databse
+  const numItemsInCart = 12;
+  return (
+    <Button
+      asChild
+      variant="outline"
+      size="icon"
+      className="flex justify-center items-center relative"
+    >
+      <Link href="/cart">
+        <FaShoppingCart />
+        <span className="absolute -top-3 -right-3 bg-primary text-white rounded-full h-6 w-6 flex items-center justify-center text-xs">
+          {numItemsInCart}
+        </span>
+      </Link>
+    </Button>
+  );
 };
 
 export default CartButton;
