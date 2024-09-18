262e63343cca0d08b337b8d10b13cf4329f49677
faroukq1 <faroukq1@gmail.com>
2024-09-18 15:11:14 +0100

    container component

---
+++ components/global/Container.tsx
@@ -0,0 +1,15 @@
+import { cn } from "@/lib/utils";
+
+type Container = {
+  children: React.ReactNode;
+  className?: string;
+};
+const Container = ({ children, className }: Container) => {
+  return (
+    <div className={cn("mx-auto max-w-6xl xl:max-w-7xl px-8", className)}>
+      {children}
+    </div>
+  );
+};
+
+export default Container;
