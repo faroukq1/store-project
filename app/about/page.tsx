84af57dc10c3a137aba8b42015638f210cdbed37
faroukq1 <faroukq1@gmail.com>
2024-09-19 15:15:13 +0100

    using prisma query

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import db from "../utils/db";
+const AboutPage = async () => {
+  const users = await db.testProfile.findMany();
 
-const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div>
+      {users.map((item, index) => {
+        return (
+          <h2 key={item.id} className="text-2xl font-bold">
+            {item.name} : {index + 1}
+          </h2>
+        );
+      })}
+    </div>
+  );
 };
 
 export default AboutPage;
