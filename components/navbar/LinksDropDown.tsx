cb80d8749d26064b7d132cc62d84863aa34ac8c0
faroukq1 <faroukq1@gmail.com>
2024-09-19 14:09:56 +0100

    navbar dropdown menu

--- components/navbar/LinksDropDown.tsx
+++ components/navbar/LinksDropDown.tsx
@@ -1,5 +1,31 @@
+import { links } from "@/app/utils/links";
+import { CiMenuBurger } from "react-icons/ci";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Link from "next/link";
+
 const LinksDropDown = () => {
-  return <div>LinksDropDown</div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <CiMenuBurger className="text-2xl" />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="mt-2" align="center" sideOffset={10}>
+        {links.map((link) => {
+          return (
+            <DropdownMenuItem asChild>
+              <Link href={link.href}>{link.label}</Link>
+            </DropdownMenuItem>
+          );
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
 
 export default LinksDropDown;
