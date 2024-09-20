48fd2dd018cd325b35cdd426277519b5aea153a0
faroukq1 <faroukq1@gmail.com>
2024-09-20 11:45:55 +0100

    hero carousel

--- components/home/HeroCarousel.tsx
+++ components/home/HeroCarousel.tsx
@@ -1,5 +1,45 @@
+import Image from "next/image";
+import image1 from "../../public/image1.jpg";
+import image2 from "../../public/image2.jpg";
+import image3 from "../../public/image3.jpg";
+import image4 from "../../public/image4.jpg";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../ui/carousel";
+import { Card, CardContent } from "../ui/card";
+
+const carouselImages = [image1, image2, image3, image4];
+
 const HeroCarousel = () => {
-  return <div>HeroCarousel</div>;
+  return (
+    <div className="hidden lg:block">
+      <Carousel>
+        <CarouselContent>
+          {carouselImages.map((image, index) => {
+            return (
+              <CarouselItem key={index}>
+                <Card>
+                  <CardContent className="p-2">
+                    <Image
+                      src={image}
+                      alt="hero"
+                      className="w-full h-[24rem] rounded-md object-cover"
+                    />
+                  </CardContent>
+                </Card>
+              </CarouselItem>
+            );
+          })}
+        </CarouselContent>
+        <CarouselNext />
+        <CarouselPrevious />
+      </Carousel>
+    </div>
+  );
 };
 
 export default HeroCarousel;
