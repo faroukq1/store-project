6089ea74d2af65383214a3b97b895e88ba027ba7
faroukq1 <faroukq1@gmail.com>
2024-09-20 11:29:16 +0100

    prduct grid

--- components/products/ProductGrid.tsx
+++ components/products/ProductGrid.tsx
@@ -3,6 +3,7 @@ import { Product } from "@prisma/client";
 import Link from "next/link";
 import { Card, CardContent } from "../ui/card";
 import Image from "next/image";
+import FavoriteToggleButton from "./FavoriteToggleButton";
 const ProductGrid = async ({ products }: { products: Product[] }) => {
   return (
     <div className="pt-12 grid gap-4 md:grid-cols-2 lg:grid-cols-3">
@@ -24,9 +25,16 @@ const ProductGrid = async ({ products }: { products: Product[] }) => {
                       className="rounded w-full object-cover transofrm group-hover:scale-110 transition-transform duration-500"
                     />
                   </div>
+                  <div className="mt-4 text-center">
+                    <h2 className="text-lg capitalize">{name}</h2>
+                    <p className="text-muted-foreground mt-2">{dollarAmount}</p>
+                  </div>
                 </CardContent>
               </Card>
             </Link>
+            <div className="absolute top-7 right-7 z-5">
+              <FavoriteToggleButton productID={id} />
+            </div>
           </article>
         );
       })}
