6a99dccbb911d2278fa15333d8b12a99c570f848
faroukq1 <faroukq1@gmail.com>
2024-09-20 11:10:27 +0100

    favorite toggle button

--- components/products/FavoriteToggleButton.tsx
+++ components/products/FavoriteToggleButton.tsx
@@ -1,5 +1,17 @@
-const FavoriteToggleButton = () => {
-  return <div>FavoriteToggleButton</div>;
+import { FaHeart } from "react-icons/fa";
+import { Button } from "../ui/button";
+
+const FavoriteToggleButton = ({ productID }: { productID: string }) => {
+  return (
+    <Button
+      size="icon"
+      variant="outline"
+      className="p-2 cursor-pointer"
+      onClick={() => console.log(productID)}
+    >
+      <FaHeart />
+    </Button>
+  );
 };
 
 export default FavoriteToggleButton;
