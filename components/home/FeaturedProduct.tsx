56ffe1197dc42d664a921eecd61b1a3220fd7ebd
faroukq1 <faroukq1@gmail.com>
2024-09-20 11:21:02 +0100

    featured products

--- components/home/FeaturedProduct.tsx
+++ components/home/FeaturedProduct.tsx
@@ -1,16 +1,16 @@
-import { fetchFeaturedProduct } from "@/app/utils/actions";
+import { fetchFeaturedProducts } from "@/app/utils/actions";
 import EmptyList from "../global/EmptyList";
 import SectionTitle from "../global/SectionTitle";
 import ProductGrid from "../products/ProductGrid";
 
 const FeaturedProduct = async () => {
-  const products = await fetchFeaturedProduct();
+  const products = await fetchFeaturedProducts();
   if (products.length === 0) return <EmptyList />;
 
   return (
     <section className="pt-24">
       <SectionTitle text="featured product" />
-      <ProductGrid products />
+      <ProductGrid products={products} />
     </section>
   );
 };
