396361e4a73b839f971b993766153c4cde1cb058
faroukq1 <faroukq1@gmail.com>
2024-09-20 10:34:15 +0100

    product model

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,16 +1,7 @@
-import db from "../utils/db";
-const AboutPage = async () => {
-  const users = await db.testProfile.findMany();
-
+const AboutPage = () => {
   return (
     <div>
-      {users.map((item, index) => {
-        return (
-          <h2 key={item.id} className="text-2xl font-bold">
-            {item.name} : {index + 1}
-          </h2>
-        );
-      })}
+      <h1>about page</h1>
     </div>
   );
 };
