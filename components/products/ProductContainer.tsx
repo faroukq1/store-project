302063e97e9dae47b28b5be20ef4997dd934e01e
faroukq1 <faroukq1@gmail.com>
2024-09-21 11:57:22 +0100

    product container

--- components/products/ProductContainer.tsx
+++ components/products/ProductContainer.tsx
@@ -1,6 +1,6 @@
 import ProductGrid from "./ProductGrid";
 import ProductList from "./ProductList";
-import { LuLayoutGrid, LuList } from "react-icons/lu";
+import { LuLayoutGrid, LuLayoutList } from "react-icons/lu";
 import { Button } from "../ui/button";
 import { Separator } from "@radix-ui/react-separator";
 import { fetchAllProduct } from "@/app/utils/actions";
@@ -19,7 +19,35 @@ const ProductContainer = async ({
   return (
     <>
       {/* HEADER */}
-      <section></section>
+      <section>
+        <div className="flex justify-between items-center">
+          <h4 className="font-medium text-lg">
+            {totalProducts} product{totalProducts > 1 && "s"}
+          </h4>
+          <div className="flex gap-x-4">
+            <Button
+              variant={layout === "grid" ? "default" : "ghost"}
+              size="icon"
+              asChild
+            >
+              <Link href={`/product/layout=grid${searchTerm}`}>
+                <LuLayoutGrid />
+              </Link>
+            </Button>
+
+            <Button
+              variant={layout === "list" ? "default" : "ghost"}
+              size="icon"
+              asChild
+            >
+              <Link href={`/product/layout=list${searchTerm}`}>
+                <LuLayoutList />
+              </Link>
+            </Button>
+          </div>
+        </div>
+        <Separator className="mt-4" />
+      </section>
       {/* PRODUCTS */}
       <div>
         {totalProducts === 0 ? (
