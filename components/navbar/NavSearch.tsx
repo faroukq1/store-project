68c08640f1f2d9b0773e87479f87ae9bdba5c4b6
faroukq1 <faroukq1@gmail.com>
2024-09-22 11:02:33 +0100

    navsearch functionality

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,11 +1,32 @@
+"use client";
 import { Input } from "../ui/input";
-
+import { useSearchParams, useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
+import { useState, useEffect } from "react";
 const NavSearch = () => {
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+  const [search, setSearch] = useState<string>(
+    searchParams.get("search")?.toString() || ""
+  );
+  const handleSearch = useDebouncedCallback((value: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (value) params.set("search", value);
+    else params.delete("search");
+
+    replace(`/products?${params}`);
+  }, 500);
   return (
     <Input
       type="search"
       placeholder="search product..."
       className="max-w-xs dark:bg-muted"
+      onChange={(e) => {
+        const { value } = e.target;
+        setSearch(value);
+        handleSearch(value);
+      }}
+      value={search}
     />
   );
 };
