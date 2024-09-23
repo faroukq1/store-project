f4878a63128aa20a7836381e2b3cc900d8b4d727
faroukq1 <faroukq1@gmail.com>
2024-09-23 11:50:32 +0100

    user avatar

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,5 +1,17 @@
-const UserIcon = () => {
-  return <div>UserIcon</div>;
+import { LuUser2 } from "react-icons/lu";
+import { currentUser } from "@clerk/nextjs/server";
+const UserIcon = async () => {
+  // you can get the user id by destruct {userId} from auth
+  const user = await currentUser();
+  const profileImage = user?.imageUrl;
+
+  if (profileImage) {
+    return (
+      <img src={profileImage} className="w-6 h-6 rounded-full object-cover" />
+    );
+  }
+
+  return <LuUser2 className="w-6 h-6 bg-primary rounded-full text-white" />;
 };
 
 export default UserIcon;
