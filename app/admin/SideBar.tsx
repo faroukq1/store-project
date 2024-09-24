1ffec6c3ac6f050cdc18c109a5cfa2072b69f184
faroukq1 <faroukq1@gmail.com>
2024-09-24 11:43:36 +0100

    side bar & dashboard links

--- app/admin/SideBar.tsx
+++ app/admin/SideBar.tsx
@@ -1,10 +1,30 @@
+"use client";
 import { adminLinks } from "../utils/links";
 import { Button } from "@/components/ui/button";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 
 const SideBar = () => {
-  return <div>SideBar</div>;
+  const pathname = usePathname();
+
+  return (
+    <aside>
+      {adminLinks.map(({ href, label }) => {
+        const isActivePage = pathname === href;
+        const variant = isActivePage ? "secondary" : "ghost";
+        return (
+          <Button
+            key={href}
+            asChild
+            variant={variant}
+            className="w-full mb-2 capitalize font-normal"
+          >
+            <Link href={href}>{label}</Link>
+          </Button>
+        );
+      })}
+    </aside>
+  );
 };
 
 export default SideBar;
