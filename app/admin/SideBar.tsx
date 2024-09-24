e5c986e584565f18f246e5a885da50d8a9aeb463
faroukq1 <faroukq1@gmail.com>
2024-09-24 11:36:10 +0100

    admin component

---
+++ app/admin/SideBar.tsx
@@ -0,0 +1,10 @@
+import { adminLinks } from "../utils/links";
+import { Button } from "@/components/ui/button";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+const SideBar = () => {
+  return <div>SideBar</div>;
+};
+
+export default SideBar;
---
+++ app/admin/products/page.tsx
@@ -0,0 +1,5 @@
+const ProductsPage = () => {
+  return <div>admin ProductsPage</div>;
+};
+
+export default ProductsPage;
