093d22f61e83769f105547e9fcdcaff8e0597524
faroukq1 <faroukq1@gmail.com>
2024-09-24 11:47:24 +0100

    log user id

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isPublicRoute = createRouteMatcher(["/", "/about", "/products(.*)"]);
 
 export default clerkMiddleware((auth, req) => {
+  console.log(auth().userId);
   if (!isPublicRoute(req)) auth().protect();
 });
 
