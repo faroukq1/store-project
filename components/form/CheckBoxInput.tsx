5b942c8209ae7d687cf529d9a586d516b00434d1
faroukq1 <faroukq1@gmail.com>
2024-09-26 14:45:35 +0100

    form components

--- components/form/CheckBoxInput.tsx
+++ components/form/CheckBoxInput.tsx
@@ -1,5 +1,27 @@
-const CheckBoxInput = () => {
-  return <div>CheckBoxInput</div>;
+"use client";
+import { Checkbox } from "../ui/checkbox";
+
+type checkboxInputProps = {
+  name: string;
+  label: string;
+  defaultChecked?: boolean;
+};
+const CheckBoxInput = ({
+  name,
+  label,
+  defaultChecked = false,
+}: checkboxInputProps) => {
+  return (
+    <div className="flex items-center space-x-2">
+      <Checkbox id={name} name={name} defaultChecked={defaultChecked} />
+      <label
+        htmlFor={name}
+        className="text-sm leading-none capitalize peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+      >
+        {label}
+      </label>
+    </div>
+  );
 };
 
 export default CheckBoxInput;
