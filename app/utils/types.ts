9104da182ff6a8cd4eebdbc899d6bcf1f37de195
faroukq1 <faroukq1@gmail.com>
2024-09-27 14:37:10 +0100

    types

--- app/utils/types.ts
+++ app/utils/types.ts
@@ -1,9 +1,5 @@
 import { Prisma } from "@prisma/client";
 
-export type CartItemWithProduct = Prisma.CartItemGetPayload<{
-  include: { product: true };
-}>;
-
 export type actionFunction = (
   prevState: any,
   formData: FormData
