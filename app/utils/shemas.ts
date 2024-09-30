f6c9d4b87b0369a99854ed3f267262855e19901b
faroukq1 <faroukq1@gmail.com>
2024-09-30 14:34:18 +0100

    new product shem

--- app/utils/shemas.ts
+++ app/utils/shemas.ts
@@ -1,9 +1,35 @@
 import { z, ZodSchema } from "zod";
 
+// export const productShema = z.object({
+//   name: z.string().min(4),
+//   company: z.string().min(5),
+//   price: z.coerce.number().int().min(0),
+//   description: z.string(),
+//   featured: z.coerce.boolean(),
+// });
+
 export const productShema = z.object({
-  name: z.string().min(4),
-  company: z.string().min(5),
-  price: z.coerce.number().int().min(0),
-  description: z.string(),
+  name: z
+    .string()
+    .min(2, {
+      message: "name must be at least 2 characters.",
+    })
+    .max(100, {
+      message: "name must be less than 100 characters.",
+    }),
+
+  company: z.string(),
   featured: z.coerce.boolean(),
+  price: z.coerce.number().int().min(0, {
+    message: "price must be positive number.",
+  }),
+  description: z.string().refine(
+    (description) => {
+      const wordCount = description.split("").length;
+      return wordCount >= 10 && wordCount <= 1000;
+    },
+    {
+      message: "description must be between 10 and 1000 words.",
+    }
+  ),
 });
